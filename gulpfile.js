befe58b1209a6db34f44c9bd39196345539ce078
Po Chen <pchen@zendesk.com>
2015-03-23 00:31:15 +1100

    Add minified version to distribution

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 
 var concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
     jshint = require('gulp-jshint'),
     ngAnnotate = require('gulp-ng-annotate');
 
@@ -14,6 +16,9 @@ gulp.task('compile', function() {
   return gulp.src('src/**/*.js')
     .pipe(ngAnnotate())
     .pipe(concat('scroll-trigger.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(uglify())
+    .pipe(rename('scroll-trigger.min.js'))
     .pipe(gulp.dest('dist'));
 });
 
